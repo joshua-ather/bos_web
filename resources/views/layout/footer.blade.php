ac229ccf0cd8deeb2b8f882077ed35318d69fae6
Joshua Ather <joshuaather5@gmail.com>
2021-02-03 18:19:10 +0700

    Fix footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="d-flex justify-content-center">
+        <span>Copyright @ 2020 Bengkel BOS - Bengkel Hemat dan Baik (PT Penta Artha Impresi). All rights reserved.</span>
+    </div>
+</footer>
